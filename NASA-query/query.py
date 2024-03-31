f828446a7dff4bd9e6695fe45e49808fb5a1b55f
Layan11 <layan.haddad0@gmail.com>
2024-03-31 20:27:02 +0300

    requirements & config

--- NASA-query/query.py
+++ NASA-query/query.py
@@ -1,6 +1,7 @@
 import urllib
 import requests
 import csv
+import configparser
 
 """
 This is a script in python which queries the NASA image and video library for all assets related to “Ilan Ramon”.
@@ -12,7 +13,7 @@ with original file size larger than 1000 kb.
 # given a dictionary of images and an index, returns the size of the corresponding image in kb
 def get_size_in_kb(jsondict, index):
     # gets the url of the image file in different sizes (small, medium, large, and original) as well as metadata
-    image_urls = requests.get(jsondict[i]['href']).json()
+    image_urls = requests.get(jsondict[index]['href']).json()
     # get the url for the image in the original size, which is in index 0 of the image_urls list
     orig_image_url = image_urls[0]
 
@@ -45,40 +46,62 @@ def create_csv_output(output_rows_list):
             writer.writerow(row)
 
 
-# this is nasa's api endpoint to perform a search
-url = "https://images-api.nasa.gov/search?q=ilan ramon"
-collection = get_collection(url)
-output_rows_list = []
-next_page = True
-page_num = 1
-images_sum = 0
+def read_config():
+    # Create a ConfigParser object
+    config = configparser.ConfigParser()
 
-# there could be multiple pages in the get response, so this while loop keeps running while there is a next page
-# to go through all the pages
-while next_page:
-    # checks if there is a next page
-    if collection['links'][0]['rel'] == 'prev':
-        next_page = False
+    # Read the configuration file
+    config.read('config.ini')
 
-    # get the next page url to go through its images in the next iteration
-    next_page_url = collection['links'][0]['href']
-    # the 'jsondict' is a dictionary that has all the images information and metadata
-    jsondict = collection['items']
-    print(f"The current page number is: {page_num}, the number of images in this page is: {len(jsondict)}")
+    # Access values from the configuration file
+    nasa_ilan_ramon_url = config.get('URLs', 'nasa_ilan_ramon_url')
 
-    # goes over all the images in the 'jsondict'
-    for i in range(len(jsondict)):
-        # get the current image size in kb
-        image_size_in_kb = get_size_in_kb(jsondict, i)
+    # Return a dictionary with the retrieved values
+    config_values = {
+        'nasa_ilan_ramon_url': nasa_ilan_ramon_url,
+    }
 
-        if image_size_in_kb > 1000:
-            images_sum += 1
-            nasa_id = jsondict[i]['data'][0]['nasa_id']
-            output_rows_list.append([nasa_id, round(image_size_in_kb)])
+    return config_values
 
-    # update the collection to the collection of the next page
-    collection = get_collection(next_page_url)
-    page_num += 1
 
-create_csv_output(output_rows_list)
-print(f"The total number of images with original file size bigger than 1000 kb is: {images_sum}")
+if __name__ == '__main__':
+    # Call the function to read the configuration file
+    config_data = read_config()
+    # retrieves the NASA's API endpoint url from config file - this is nasa's api endpoint to perform a search
+    url = config_data['nasa_ilan_ramon_url']
+    collection = get_collection(url)
+    output_rows_list = []
+    next_page = True
+    page_num = 1
+    images_sum = 0
+
+    # there could be multiple pages in the get response, so this while loop keeps running while there is a next page
+    # to go through all the pages
+    while next_page:
+        # checks if there is a next page
+        if collection['links'][0]['rel'] == 'prev':
+            next_page = False
+
+        # get the next page url to go through its images in the next iteration
+        next_page_url = collection['links'][0]['href']
+        # the 'jsondict' is a dictionary that has all the images information and metadata
+        jsondict = collection['items']
+        print(f"The current page number is: {page_num}, the number of images in this page is: {len(jsondict)}")
+
+        # goes over all the images in the 'jsondict'
+        for i in range(len(jsondict)):
+            print(f"Current image number is: {i + 1}")
+            # get the current image size in kb
+            image_size_in_kb = get_size_in_kb(jsondict, i)
+
+            if image_size_in_kb > 1000:
+                images_sum += 1
+                nasa_id = jsondict[i]['data'][0]['nasa_id']
+                output_rows_list.append([nasa_id, round(image_size_in_kb)])
+
+        # update the collection to the collection of the next page
+        collection = get_collection(next_page_url)
+        page_num += 1
+
+    create_csv_output(output_rows_list)
+    print(f"The total number of images with original file size bigger than 1000 kb is: {images_sum}")
